4749949a933d62d3f78550753395a1956cf9824b
EntranceJew <EntranceJew@gmail.com>
2023-09-15 16:29:20 -0500

    collusionist role disguise works

--- lua/terrortown/entities/roles/collusionist/shared.lua
+++ lua/terrortown/entities/roles/collusionist/shared.lua
@@ -14,7 +14,7 @@ function ROLE:PreInitialize()
 	self.scoreKillsMultiplier = 1 -- multiplier for kill of player of another team
 	self.scoreTeamKillsMultiplier = -8 -- multiplier for teamkill
 	self.preventWin = true -- set true if role can't win (maybe because of own / special win conditions)
-	self.defaultTeam = TEAM_COLLUSIONIST -- the team name: roles with same team name are working together
+	self.defaultTeam = TEAM_JESTER -- the team name: roles with same team name are working together
 	self.defaultEquipment = SPECIAL_EQUIPMENT -- here you can set up your own default equipment
 
 	self.conVarData = {
@@ -48,7 +48,14 @@ if SERVER then
 	CreateConVar("ttt2_collusionist_reveal_mode", "0", {FCVAR_NOTIFY, FCVAR_ARCHIVE})
 
 ---@diagnostic disable-next-line: param-type-mismatch
+	CreateConVar("ttt2_col_appear_as_innocent_to_evils", "1", {FCVAR_NOTIFY, FCVAR_ARCHIVE})
+---@diagnostic disable-next-line: param-type-mismatch
+	CreateConVar("ttt2_col_appear_as_jester", "1", {FCVAR_NOTIFY, FCVAR_ARCHIVE})
+
+---@diagnostic disable-next-line: param-type-mismatch
 	CreateConVar("ttt2_collusionist_kill_policing_roles", "1", {FCVAR_NOTIFY, FCVAR_ARCHIVE})
+---@diagnostic disable-next-line: param-type-mismatch
+	CreateConVar("ttt2_collusionist_patch_radar", "1", {FCVAR_NOTIFY, FCVAR_ARCHIVE})
 end
 
 if CLIENT then
@@ -90,6 +97,23 @@ if CLIENT then
 			serverConvar = "ttt2_collusionist_respawn",
 			label = "label_collusionist_respawn",
 		})
+		form:MakeHelp({
+			label = "help_col_appear_as_innocent_to_evils"
+		})
+		form:MakeCheckBox({
+			serverConvar = "ttt2_col_appear_as_innocent_to_evils",
+			label = "label_col_appear_as_innocent_to_evils",
+		})
+		form:MakeHelp({
+			label = "help_col_appear_as_jester"
+		})
+		form:MakeCheckBox({
+			serverConvar = "ttt2_col_appear_as_jester",
+			label = "label_col_appear_as_jester",
+		})
+		form:MakeHelp({
+			label = "help_collusionist_respawn_delay"
+		})
 		form:MakeSlider({
 			serverConvar = "ttt2_collusionist_respawn_delay",
 			min = 0,
@@ -97,21 +121,27 @@ if CLIENT then
 			decimal = 0,
 			label = "label_collusionist_respawn_delay"
 		})
-		form:MakeComboBox({
-			serverConvar = "ttt2_collusionist_reveal_mode",
-			label = "label_collusionist_reveal_mode",
-			choices = {
-				"0 - Never reveal the collusionist has changed team",
-				"1 - Only alert the detective or traitors the collusionist has now joined",
-				"2 - Alert all of the collusionists new team members",
-				"3 - Alert everyone of the collusionists new team",
-			},
-			default = 0,
-		})
-
+		-- form:MakeComboBox({
+		-- 	serverConvar = "ttt2_collusionist_reveal_mode",
+		-- 	label = "label_collusionist_reveal_mode",
+		-- 	choices = {
+		-- 		{value = 0, title = "0 - Never reveal the collusionist has changed team"},
+		-- 		{value = 1, title = "1 - Only alert the detective or traitors the collusionist has now joined"},
+		-- 		{value = 2, title = "2 - Alert all of the collusionists new team members"},
+		-- 		{value = 3, title = "3 - Alert everyone of the collusionists new team"},
+		-- 	},
+		-- 	selectValue = 2,
+		-- })
 		form:MakeCheckBox({
 			serverConvar = "ttt2_collusionist_kill_policing_roles",
 			label = "label_collusionist_kill_policing_roles"
+		})
+		form:MakeHelp({
+			label = "help_collusionist_patch_radar"
+		})
+		form:MakeCheckBox({
+			serverConvar = "ttt2_collusionist_patch_radar",
+			label = "label_collusionist_patch_radar",
 		})
 	end
 end
@@ -177,30 +207,67 @@ if SERVER then
 		)
 	end
 
-	hook.Add("TTT2SpecialRoleSyncing", "TTT2RoleCollusionist", function(ply, tbl)
-		if ply and not ply:HasTeam(TEAM_TRAITOR) or ply:GetSubRoleData().unknownTeam or GetRoundState() == ROUND_POST then return end
+	local appear_role = function(plySyncTo, plySyncFrom)
+		local is_from_col = plySyncFrom:GetSubRole() == ROLE_COLLUSIONIST
 
-		for collusionist in pairs(tbl) do
-			if not collusionist:IsTerror() or collusionist == ply then
-				continue
+		local col_as_jes = GetConVar("ttt2_col_appear_as_jester"):GetBool()
+		local evils_are_not_knowers = GetConVar("ttt2_col_appear_as_innocent_to_evils"):GetBool()
+		local is_to_evil = plySyncTo:HasTeam() and plySyncTo:GetTeam() == TEAM_TRAITOR
+
+		if is_from_col and is_to_evil then
+			if evils_are_not_knowers then
+				return {ROLE_NONE, TEAM_NONE}
 			end
-			if ply:GetSubRole() ~= ROLE_COLLUSIONIST and collusionist:GetSubRole() == ROLE_COLLUSIONIST then
-				if not collusionist:Alive() then
-					continue
-				end
-				if ply:GetTeam() ~= TEAM_JESTER then
-					tbl[collusionist] = {ROLE_JESTER, TEAM_JESTER}
-				else
-					tbl[collusionist] = {ROLE_COLLUSIONIST, TEAM_JESTER}
-				end
+
+			if col_as_jes then
+				return {ROLE_JESTER, TEAM_JESTER}
+			-- else
+				-- return {ROLE_COLLUSIONIST, TEAM_JESTER}
 			end
 		end
-	end)
+	end
+	local appear_role_radar = function(plySyncTo, plySyncFrom)
+		local is_from_col = plySyncFrom:GetSubRole() == ROLE_COLLUSIONIST
 
-	hook.Add("TTT2ModifyRadarRole", "TTT2ModifyRadarRoleCollusionist", function(ply, target)
-		if ply:HasTeam(TEAM_TRAITOR) and target:GetSubRole() == ROLE_COLLUSIONIST then
-			return ROLE_JESTER, TEAM_JESTER
+		local col_as_jes = GetConVar("ttt2_col_appear_as_jester"):GetBool()
+		local evils_are_not_knowers = GetConVar("ttt2_col_appear_as_innocent_to_evils"):GetBool()
+		local is_to_evil = plySyncTo:HasTeam() and plySyncTo:GetTeam() == TEAM_TRAITOR
+
+		if is_from_col and is_to_evil then
+			if evils_are_not_knowers then
+				return {ROLE_INNOCENT, TEAM_INNOCENT}
+			end
+
+			if col_as_jes then
+				return {ROLE_JESTER, TEAM_JESTER}
+			else
+				return {ROLE_COLLUSIONIST, TEAM_JESTER}
+			end
 		end
+	end
+
+	hook.Add("PostInitPostEntity", "TTT2CollusionistPatchJesterRadar", function()
+		if GetConVar("ttt2_collusionist_patch_radar"):GetBool() then
+			local function ShouldShowJesterToTeam(ply)
+				return (GetConVar("ttt2_jes_exppose_to_all_evils"):GetBool() and ply:GetTeam() ~= TEAM_INNOCENT and not ply:GetSubRoleData().unknownTeam)
+					or (not GetConVar("ttt2_jes_exppose_to_all_evils"):GetBool() and ply:GetTeam() == TEAM_TRAITOR)
+			end
+
+			hook.Remove("TTT2ModifyRadarRole", "TTT2ModifyRadarRoleJester")
+			hook.Add("TTT2ModifyRadarRole", "TTT2ModifyRadarRoleJester", function(ply, target)
+				if not target:GetTeam() == TEAM_JESTER or not ShouldShowJesterToTeam(ply) then return end
+
+				local roleAndTeam = hook.Run("TTT2JesterModifyRadarRole", ply, target)
+
+				if roleAndTeam then
+					return roleAndTeam[1], roleAndTeam[2]
+				end
+			end)
+			hook.Add("TTT2JesterModifyRadarRole", "CollusionistHideAsJester", appear_role_radar)
+		end
+
+		hook.Remove("TTT2JesterModifySyncedRole", "CollusionistHideAsJester")
+		hook.Add("TTT2JesterModifySyncedRole", "CollusionistHideAsJester", appear_role)
 	end)
 
 	hook.Add("WeaponEquip", "CollusionistItemEquip", function(weapon, ply)
@@ -214,7 +281,7 @@ if SERVER then
 
 				print("Collusionist has picked up a " .. tostring(weapon) .. " dropped by " .. tostring(donator) .. " who has this team " .. tostring(team))
 
-				if not donator:IsActive() or (GetConVar("ttt2_collusionist_kill_policing_roles"):GetBool() and donator:GetSubRoleData().isPolicingRole) then return end
+				if not donator:IsActive() or (not GetConVar("ttt2_collusionist_kill_policing_roles"):GetBool() and donator:GetSubRoleData().isPolicingRole) then return end
 
 				donator:PrintMessage(HUD_PRINTTALK, "You have colluded with the collusionist!")
 				-- donator:PrintMessage(HUD_PRINTCENTER, "You have colluded with the collusionist!")
@@ -267,14 +334,14 @@ if SERVER then
 				local delay = GetConVar("ttt2_collusionist_respawn_delay"):GetInt()
 				if delay > 0 then
 					victim:PrintMessage(HUD_PRINTTALK, "You were killed but will respawn in " .. delay .. " seconds.")
-				else
-					victim:PrintMessage(HUD_PRINTTALK, "You were killed but are about to respawn.")
+					timer.Simple(delay, function()
+						CollusionistRevive(victim)
+					end)
+				-- else
+					-- victim:PrintMessage(HUD_PRINTTALK, "You were killed and will not respawn.")
 					-- Introduce a slight delay to prevent player getting stuck as a spectator
-					delay = 0.1
+					-- delay = 0.1
 				end
-				timer.Simple(delay, function()
-					CollusionistRevive(victim)
-				end)
 			end
 		end
 	end)
--- lua/terrortown/lang/en/collusionist.lua
+++ lua/terrortown/lang/en/collusionist.lua
@@ -9,13 +9,22 @@ L["target_" .. COLLUSIONIST.name] = "Collusionist"
 L["ttt2_desc_" .. COLLUSIONIST.name] = [[The collusionist is a jester role that will be converted to whoevers team they recieve a bought item from!]]
 
 -- OTHER ROLE LANGUAGE STRINGS
-L["label_collusionist_donor_health"] = ""
-L["label_collusionist_respawn_health"] = ""
+L["label_collusionist_donor_health"] = "Donor Health"
+L["help_collusionist_donor_health"] = "The health that someone that drops an item for the collusionist will respawn with."
+L["label_collusionist_respawn_health"] = "Respawn Health"
+L["help_collusionist_respawn_health"] = "The amount of health the collusionist should respawn with."
+L["label_collusionist_kill_policing_roles"] = "Kill Police?"
+L["label_col_appear_as_innocent_to_evils"] = "Appear As Innocent To Evils"
+L["help_col_appear_as_innocent_to_evils"] = "Makes it so that if a jester would ordinarily be revealed, that it instead goes back to an unknown role."
+L["label_col_appear_as_jester"] = "Appear As Jester"
+L["help_col_appear_as_jester"] = "Makes it so that if someone would know someone is the collusionist, that they instead appear as a jester."
 
 L["label_collusionist_entity_damage"] = "Can the collusionist damage entities? (Def. 1)"
 L["label_collusionist_environmental_damage"] = "Can explode, burn, crush, fall, drown? (Def. 1)"
 L["label_collusionist_respawn"] = "Collusionist respawn on death (Def. 1)"
 L["label_collusionist_respawn_delay"] = "Collusionist respawn delay (Def. 3)"
+L["help_collusionist_respawn_delay"] = "The time it takes for the collusionist to be revived.\nSet to \"0\" to prevent respawning."
 L["label_collusionist_reveal_mode"] = "ttt2_collusionist_reveal_mode (Def: 0)"
 
-L["label_collusionist_kill_policing_roles"] = ""
+L["label_collusionist_patch_radar"] = "Fix: Patch Radar"
+L["help_collusionist_patch_radar"] = "Allows patching the radar so that returning ROLE_NONE doesn't cause a visual discrepency.\nYou will need this for \"Appear As Innocent To Evils\".\nApplies after mapchange."
